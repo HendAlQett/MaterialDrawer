430c97094bef041e14490ca648996679a00df6af
Hend <cathe.2007@gmail.com>
2016-07-23 05:24:39 +0200

    feat: helper classes for drag and swipe

---
+++ app/src/main/java/helper/OnStartDragListener.java
@@ -0,0 +1,10 @@
+package helper;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by hend on 7/12/16.
+ */
+public interface OnStartDragListener {
+   void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
